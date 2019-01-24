a445b01eef57f601187771a83e27ca7f80441930
Jennifer Parra <jparra.biblio@gmail.com>
2019-01-24 18:46:40 -0300

    ultimo commit antes del deploy

--- main.js
+++ main.js
@@ -20,42 +20,45 @@ const dataThemes =['tt0896986', 'tt0097240', 'tt0120788', 'tt0112461', 'tt018186
 
 
         document.getElementById('root').innerHTML += 
-        `<div class="card text-center">
-             <div class="card-header">
-                 <ul class="nav nav-tabs card-header-tabs">
-                     <li class="nav-item">
-                         <a class="nav-link active" href="#">Ficha</a>
-                     </li>
-                     <li class="nav-item">
-                         <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Ver comentarios</a>
-                     </li>
-                 </ul>
-             </div>
-             <div class="card-body">
-                 <div class="container">
-                     <!-- Stack the columns on mobile by making one full-width and the other half-width -->
-                     <div class="row">
-                         <div class="col-4"><img src="${data.Poster}" alt="" class="img-thumbnail"></div>
-                         <div class="col-8">
-                             <div class="col-12">
-                                 <h5>${data.Title}</h5>
-                             </div>
-                             <p><span class="bold">País:</span> ${data.Country}</p>
-                             <p><span class="bold">Año:</span> ${data.Year}</p>
-                             <p><span class="bold">Género:</span> ${data.Genre}</p>
-                             <p><span class="bold">Duración:</span> ${data.Runtime}</p>
-                             <p><span class="bold">Sinopsis:</span>${data.Plot}</p>
-                             <div class="row">
-                             <div class="col-4">
-                                     <p class="bold">Ingresa un comentario:</p>
-                                 </div>
-                             <div class="col-8 form-group">
-                                 <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Escribe tu opinión para que el resto de los usuarios la pueda leer."></textarea>
-                                 <button class="btn btn-warning mt-3">Agregar</button>
-                             </div>
-                             <p><span class="bold">Tags: <span class="badge badge-primary">Drogadicción</span> </span>
-                             
-                         </div> `  
+        ` <div class="card text-center">
+        <div class="card-header">
+            <ul class="nav nav-tabs card-header-tabs">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Ficha</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Ver comentarios</a>
+                </li>
+            </ul>
+        </div>
+        <div class="card-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-4"><img src="${data.Poster}" alt="" class="img-thumbnail"></div>
+                    <div class="align-left col-8 pl-2">
+                        <h5 class="py-2 title-movie">${data.Title.toUpperCase()}</h5>
+                        <p><span class="bold">País:</span> ${data.Country}</p>
+                        <p><span class="bold">Año:</span> ${data.Year}</p>
+                        <p><span class="bold">Género:</span> ${data.Genre}</p>
+                        <p><span class="bold">Duración:</span> ${data.Runtime}</p>
+                        <p><span class="bold">Sinopsis:</span> ${data.Plot}</p>
+                        <p><span class="bold">Etiquetas: <span class="badge badge-danger">Drogadicción</span> </span>
+                            <div class="row">
+                                <div class="col-12">
+                                    <p class="bold pt-2">Ingresa un comentario:</p>
+                                </div>
+                                <div class="col-12 form-group">
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
+                                        placeholder="Escribe tu opinión para que el resto de los usuarios la pueda leer."></textarea>
+                                    <button class="btn mt-3">Agregar</button>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`
+ 
       
    })
    
@@ -79,7 +82,45 @@ function socialMovie2 () {dataThemes2.map( function(item) {
     .then(res => res.json())
      .then(data => {
   
-           document.getElementById("root2").innerHTML += `<img src=${data.Poster}>  ${data.Title} ${data.Year} ${data.Type}`
+        document.getElementById('root2').innerHTML += 
+        ` <div class="card text-center">
+        <div class="card-header">
+            <ul class="nav nav-tabs card-header-tabs">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Ficha</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Ver comentarios</a>
+                </li>
+            </ul>
+        </div>
+        <div class="card-body">
+            <div class="container">
+                <div class="row">
+                    <div class="col-4"><img src="${data.Poster}" alt="" class="img-thumbnail"></div>
+                    <div class="align-left col-8 pl-2">
+                        <h5 class="py-2 title-movie">${data.Title.toUpperCase()}</h5>
+                        <p><span class="bold">País:</span> ${data.Country}</p>
+                        <p><span class="bold">Año:</span> ${data.Year}</p>
+                        <p><span class="bold">Género:</span> ${data.Genre}</p>
+                        <p><span class="bold">Duración:</span> ${data.Runtime}</p>
+                        <p><span class="bold">Sinopsis:</span> ${data.Plot}</p>
+                        <p><span class="bold">Etiquetas: <span class="badge badge-danger">Drogadicción</span> </span>
+                            <div class="row">
+                                <div class="col-12">
+                                    <p class="bold pt-2">Ingresa un comentario:</p>
+                                </div>
+                                <div class="col-12 form-group">
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
+                                        placeholder="Escribe tu opinión para que el resto de los usuarios la pueda leer."></textarea>
+                                    <button class="btn mt-3">Agregar</button>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`
      })
      
   })
@@ -103,7 +144,45 @@ const dataThemes3 = ['tt0114906', 'tt2543472', 'tt0929632', 'tt0429589', 'tt1288
           .then(res => res.json())
           .then(data => {
         
-                document.getElementById("root3").innerHTML += `<img src=${data.Poster}>  ${data.Title} ${data.Year} ${data.Type}, ${data.imdbRating}`
+            document.getElementById('root3').innerHTML += 
+            ` <div class="card text-center">
+            <div class="card-header">
+                <ul class="nav nav-tabs card-header-tabs">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Ficha</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Ver comentarios</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="card-body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-4"><img src="${data.Poster}" alt="" class="img-thumbnail"></div>
+                        <div class="align-left col-8 pl-2">
+                            <h5 class="py-2 title-movie">${data.Title.toUpperCase()}</h5>
+                            <p><span class="bold">País:</span> ${data.Country}</p>
+                            <p><span class="bold">Año:</span> ${data.Year}</p>
+                            <p><span class="bold">Género:</span> ${data.Genre}</p>
+                            <p><span class="bold">Duración:</span> ${data.Runtime}</p>
+                            <p><span class="bold">Sinopsis:</span> ${data.Plot}</p>
+                            <p><span class="bold">Etiquetas: <span class="badge badge-danger">Drogadicción</span> </span>
+                                <div class="row">
+                                    <div class="col-12">
+                                        <p class="bold pt-2">Ingresa un comentario:</p>
+                                    </div>
+                                    <div class="col-12 form-group">
+                                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
+                                            placeholder="Escribe tu opinión para que el resto de los usuarios la pueda leer."></textarea>
+                                        <button class="btn mt-3">Agregar</button>
+                                    </div>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
           })
         })
       }
@@ -119,14 +198,52 @@ document.getElementById("migration").addEventListener("click", () =>{
  })
 
 
-const dataThemes4 = ['tt0414413', 'tt0857191', 'tt1508675', 'tt6776106', 'tt5222918', 'tt3147312', 'tt1127715', 'tt0362227', 'tt0796307', 'tt0211815', 'tt0286499', 'tt3399024', 'tt2510028', 'tt4082068', 'tt0212826', 'tt0488798', 'tt1630027', 'tt0221799', 'tt0193167', 'The Immigrant']
+const dataThemes4 = ['tt0414413', 'tt0857191', 'tt1508675', 'tt6776106', 'tt5222918', 'tt3147312', 'tt1127715', 'tt0362227', 'tt0796307', 'tt0286499', 'tt3399024', 'tt2510028', 'tt4082068', 'tt0212826', 'tt0488798', 'tt1630027', 'tt0221799', 'tt0193167', 'The Immigrant']
 
         function socialMovie4 () {dataThemes4.map( function(item) {
           fetch('https://www.omdbapi.com/?i='+ item  + '&apikey=6e2550ac')
             .then(res => res.json())
             .then(data => {
           
-                  document.getElementById("root4").innerHTML += `<img src=${data.Poster}>  ${data.Title} ${data.Year} ${data.Type}, ${data.imdbRating}`
+                document.getElementById('root3').innerHTML += 
+                ` <div class="card text-center">
+                <div class="card-header">
+                    <ul class="nav nav-tabs card-header-tabs">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="#">Ficha</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Ver comentarios</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card-body">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-4"><img src="${data.Poster}" alt="" class="img-thumbnail"></div>
+                            <div class="align-left col-8 pl-2">
+                                <h5 class="py-2 title-movie">${data.Title.toUpperCase()}</h5>
+                                <p><span class="bold">País:</span> ${data.Country}</p>
+                                <p><span class="bold">Año:</span> ${data.Year}</p>
+                                <p><span class="bold">Género:</span> ${data.Genre}</p>
+                                <p><span class="bold">Duración:</span> ${data.Runtime}</p>
+                                <p><span class="bold">Sinopsis:</span> ${data.Plot}</p>
+                                <p><span class="bold">Etiquetas: <span class="badge badge-danger">Drogadicción</span> </span>
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <p class="bold pt-2">Ingresa un comentario:</p>
+                                        </div>
+                                        <div class="col-12 form-group">
+                                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"
+                                                placeholder="Escribe tu opinión para que el resto de los usuarios la pueda leer."></textarea>
+                                            <button class="btn mt-3">Agregar</button>
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`
             })
           })
 
