4abf6ebf07992ba4fb593243290a67f1736f78f0
Jennifer Parra <jparra.biblio@gmail.com>
2019-01-24 08:37:49 -0300

    cambio a bootstrap, estructura de sección drogadicción con card 12 columnas

--- main.js
+++ main.js
@@ -1,44 +1,11 @@
 
-// window.onload= movies
-
-// let movie=""
-//  function movies () {
-
-//    fetch("https://api.themoviedb.org/3/discover/movie?api_key=48819a4f88e3d597df63bebab6723d0f&with_genres=10749")
-//    .then(data=>data.json())
-//    .then(data=>{
-//      let social= data.results;
-// //    console.log(data) 
-//      for (let i = 0; i <social.length; i++){
-//        let title= social[i].title
-//         fetch ("http://www.omdbapi.com/?s="+title+"&page1=&apikey=6e2550ac")
-//           .then (data=>data.json())
-//           .then (data =>{ 
-//             movie=data
-//         //    console.log(movie) 
-//         document.getElementById("crime").addEventListener("click", () =>{
-//            document.getElementById("root").innerHTML +=  `<img src=${data.Search[0].Poster}>  ${data.Search[0].Title} ${data.Search[0].Year} ${data.Search[0].Type}`
-//            document.getElementById("root_one").style.display=none;
-//            documen
-//           })
-
-//         })
-
-//        }
-//      });
-//     };
-   
-
-
-
-
  //peliculas sobre drogadicción
 
  document.getElementById("drug").addEventListener("click", () => {
  socialMovie ()
  document.getElementById("cover_page").style.display = "none";
  document.getElementById("root2").style.display = "none";
- document.getElementById("root").style.display = "block";
+ document.getElementById("drogadiccion").style.display = "block";
 })
 
 
@@ -49,29 +16,31 @@ fetch('https://www.omdbapi.com/?i='+ item  + '&apikey=6e2550ac')
   .then(res => res.json())
    .then(data => {
 
-<<<<<<< HEAD
-         document.getElementById('root').innerHTML += `<img src=${data.Poster}>  ${data.Title} ${data.Year} ${data.Type}`
-       /*   `
-        <div class="col-3 card">
-          <img src="${data.Poster}" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">${data.Title}</h5>
-            <p class="card-text">Año: ${data.Year}</p>
-            <p class='card-text">Tipo:${data.Type}</p>
+         document.getElementById('root').innerHTML += /* `<img src=${data.Poster}>  ${data.Title} ${data.Year} ${data.Type}` */
+       
+        ` <div class="col s12 m4 card medium hoverable">
+          <div class="card-image waves-effect waves-block waves-light">  
+            <img class="activator caja-pokemon" src="${data.Poster}">
           </div>
-        </div>
-        
-      ` */
-=======
-         document.getElementById("root").innerHTML += `<img src=${data.Poster}>  ${data.Title} ${data.Year} ${data.Type}`
->>>>>>> upstream/master
+          <div class="card-content">
+          
+          <span class="card-title activator grey-text text-darken-4"><span class="bold">${data.Title}</span><i class="material-icons right">add</i></span>
+          <p><span class="bold">País:</span> ${data.Country}</p>
+          <p><span class="bold">Año:</span> ${data.Year}</p>
+          <p><span class="bold">Género:</span> ${data.Genre}</p>
+          <p><span class="bold">Duración:</span> ${data.Runtime}</p>
+          </div>
+          <div class="card-reveal grey lighten-2">
+            <span class="card-title grey-text text-darken-4"><span class="bold grey-text">Sinopsis</span><i class="material-icons right">close</i></span>
+            <p>${data.Plot}</p>
+          </div>
+        </div>`  
+      
    })
    
 })
 }
-<<<<<<< HEAD
 /* const dataThemes2 = ['tt0995829', 'tt0467406', ]
-=======
 
 // aqui se muestran las peliculas sobre sexualidad
 
@@ -81,7 +50,6 @@ fetch('https://www.omdbapi.com/?i='+ item  + '&apikey=6e2550ac')
   document.getElementById("root2").style.display = "block";
  })
 
->>>>>>> upstream/master
 
 const dataThemes2 = ['tt0995829', 'tt0467406', 'tt1847731', 'tt1659337', 'tt0159097']
 
@@ -92,17 +60,17 @@ function socialMovie2 () {dataThemes2.map( function(item) {
   
            document.getElementById("root2").innerHTML += `<img src=${data.Poster}>  ${data.Title} ${data.Year} ${data.Type}`
      })
-<<<<<<< HEAD
      
   }) */
 
-  document.getElementById('drug').addEventListener("click", () =>{
+
+  
+/*   document.getElementById('drug').addEventListener("click", () =>{
     socialMovie ()
     document.getElementById("cover_page").style.display = "none";
     document.getElementById("root").style.display = "block";
-=======
   })
-}
+
 
 
 document.getElementById("bullying").addEventListener("click", () =>{
@@ -120,9 +88,8 @@ function socialMovie3 () {dataThemes.map( function(item) {
   
            document.getElementById("root3").innerHTML += `<img src=${data.Poster}>  ${data.Title} ${data.Year} ${data.Type}`
      })
->>>>>>> upstream/master
   })
-}
+} */
 
 
   
